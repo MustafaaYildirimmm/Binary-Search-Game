b730c2c643d607c3517cd84c72a914f602b9459f
Mustafa Yıldırım <39571054+rootercs@users.noreply.github.com>
2018-10-02 02:54:09 +0300

    Project added

---
+++ NumberSearchGame(Consol)/NumberSearcgGame(Consol)/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberSearchGame_Consol_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int randomNumber , guessNumber, hak = 10;
+            char c;
+            
+            try
+            {
+                do
+                {
+                    Console.Clear();
+                    randomNumber = rnd.Next(0, 51);
+                    Console.WriteLine("bir sayi tahmin ediniz.");
+                    do
+                    {
+                        guessNumber = Convert.ToInt32(Console.ReadLine());
+                        hak--;
+                        if (hak == 0)
+                        {
+                            Console.WriteLine("hakkiniz bitmiştir");
+                            break;
+                        }
+                        else if (guessNumber > randomNumber)
+                        {
+                            Console.WriteLine("daha kücük bir sayi giriniz");
+                        }
+                        else if (guessNumber < randomNumber)
+                        {
+                            Console.WriteLine("daha buyuk bir sayi giriniz");
+                        }
+                        else
+                        {
+                            Console.WriteLine("tebrikler {0}. hakkinizda buldunuz", (10 - hak));
+                            break;
+                        }
+
+
+                    } while (true);
+                    Console.WriteLine("tekrar oynamak istermisiniz e/h");
+                    c = Convert.ToChar(Console.ReadLine());
+                    if (c == 'e')
+                    {
+                        hak = 10;
+                    }
+                    else if (c == 'h')
+                        Environment.Exit(1);
+                } while (true);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
